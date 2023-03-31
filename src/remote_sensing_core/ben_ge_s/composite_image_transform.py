60bd83ec1fc3dd3d73f56b1dd4353f36f986201c
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-03-31 17:58:46 +0200

    Added composite_image_transform.py

---
+++ src/remote_sensing_core/ben_ge_s/composite_image_transform.py
@@ -0,0 +1,38 @@
+from typing import Dict, Optional
+
+import torch
+from torch import nn
+
+from torchvision import transforms
+from torchvision.transforms import Pad
+
+
+class CompositeImageTransform(nn.Module):
+    def __init__(
+            self,
+            padding_parameters: Optional[Dict] = None,
+    ):
+        super().__init__()
+        transforms_list = []
+        if padding_parameters:
+            transforms_list.append(Pad(
+                **padding_parameters
+            ))
+        self._transform = transforms.Compose(
+            transforms_list
+        )
+
+    def forward(self, x):
+        return self._transform(x)
+
+
+if __name__ == '__main__':
+    img = torch.rand(
+        size=(3, 120, 120)
+    )
+    comp_trans = CompositeImageTransform(
+        padding_parameters={
+            "padding": 4
+        }
+    )
+    print(comp_trans(img).shape)
