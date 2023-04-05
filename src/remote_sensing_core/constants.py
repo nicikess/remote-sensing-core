1f65c19c4e413a7b31d41e533da9c65fff5201bd
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-04-05 17:36:08 +0200

    Reworked ben ge s dataset class

--- src/remote_sensing_core/constants.py
+++ src/remote_sensing_core/constants.py
@@ -3,12 +3,17 @@ from enum import Enum
 
 class Bands(Enum):
     RGB = "RGB"
-    INFRARED = "infrared"
-    ALL = "all"
+    INFRARED = "INFRARED"
+    ALL = "ALL"
 
 
+# Images
 S1_IMG_KEY = "s1_img"
 S2_IMG_KEY = "s2_img"
 WORLD_COVER_IMG_KEY = "world_cover_img"
-LABEL_KEY = "label"
+STACKED_IMAGE_KEY = "stacked_img"
+# Labels
+MULTICLASS_LABEL_KEY = "multiclass_label"
+
+# File data type
 NUMPY_DTYPE = "float32"
