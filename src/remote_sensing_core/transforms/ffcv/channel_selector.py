7b41260d9474a14f2e567db6344e5b06c9096846
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-05-02 14:55:56 +0200

    Added minor improvements to channel_selector.py

--- src/remote_sensing_core/transforms/ffcv/channel_selector.py
+++ src/remote_sensing_core/transforms/ffcv/channel_selector.py
@@ -14,7 +14,6 @@ from remote_sensing_core.constants import Bands
 
 
 class ChannelSelector(Operation):
-
     def __init__(self, s2_bands: Union[str, Bands]):
         self.band_names = Bands(s2_bands)
 
@@ -27,23 +26,34 @@ class ChannelSelector(Operation):
                 rgb_channels = [3, 2, 1]
                 rgb_channels = np.array(rgb_channels, dtype=np.int64)
                 images = images[:, rgb_channels, :, :]
-            if bands == Bands.INFRARED:
+            elif bands == Bands.INFRARED:
                 infrared_channels = [7, 3, 2, 1]
                 infrared_channels = np.array(infrared_channels, dtype=np.int64)
                 images = images[:, infrared_channels, :, :]
-            return images
+            elif bands == Bands.ALL:
+                return images
+            else:
+                raise NotImplementedError(f"Bands {bands} not implemented")
 
         return select_channels
 
-    def declare_state_and_memory(self, previous_state: State) -> Tuple[State, Optional[AllocationQuery]]:
-
+    def declare_state_and_memory(
+        self, previous_state: State
+    ) -> Tuple[State, Optional[AllocationQuery]]:
+        # Get original shape
         c, h, w = previous_state.shape
 
-        shape = (12, h, w)
+        # Update channels
+        channels = c
         if self.band_names == Bands.RGB:
-            shape = (3, h, w)
-        if self.band_names == Bands.INFRARED:
-            shape = (4, h, w)
+            channels = 3
+        elif self.band_names == Bands.INFRARED:
+            channels = 4
+        elif self.band_names == Bands.ALL:
+            assert channels == 12
+        else:
+            raise NotImplementedError(f"Bands {self.bands} not implemented")
+        shape = (channels, h, w)
 
         # Update state shape
         new_state = replace(previous_state, shape=shape)
