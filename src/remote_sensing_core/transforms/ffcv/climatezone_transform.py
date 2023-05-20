65b59c157446bc865c293a3cc1284ab84312563d
Nicolas Kesseli <nicolaskesseli@gmail.com>
2023-05-20 18:50:05 +0200

    Add climate zone transform

--- src/remote_sensing_core/transforms/ffcv/climatezone_transform.py
+++ src/remote_sensing_core/transforms/ffcv/climatezone_transform.py
@@ -1,40 +1,38 @@
-from typing import Callable, Tuple, Optional, Union
+from typing import Callable, Tuple, Optional
 
-# Numpy
-import numpy as np
-from dataclasses import replace
 
 # FFCV
 from ffcv.pipeline.operation import Operation
 from ffcv.pipeline.allocation_query import AllocationQuery
 from ffcv.pipeline.state import State
 
+from remote_sensing_core.constants import TEMPERATURE_S2_INDEX
 
-class Remove1dChannel(Operation):
-    def __init__(self, axis: int = 1):
-        self.axis = axis
+import numpy as np
+from dataclasses import replace
+
+class Era5TemperatureS2Transform(Operation):
+    def __init__(self):
+        # values taken from ben-ge_era-5.csv from ben-ge-100
+        self.number_of_climate_zones = 30
 
     def generate_code(self) -> Callable:
         # get local variables to use in return function
-        axis = self.axis
+        number_of_climate_zones = self.number_of_climate_zones
 
-        def remove_1d_channel(images, *args):
-            images = images[:, 0]
-            return images
+        def climate_zone_transform(batched_data, *args):
+            return np.eye(number_of_climate_zones)[batched_data.flatten()]
 
-        return remove_1d_channel
+        return climate_zone_transform
 
     def declare_state_and_memory(
         self, previous_state: State
     ) -> Tuple[State, Optional[AllocationQuery]]:
-
-        c = previous_state.shape
-        new_shape = ()
+        new_shape = self.number_of_climate_zones
 
         # Update state shape
         new_state = replace(previous_state, shape=new_shape)
 
         # Allocate memory for new image
         mem_allocation = AllocationQuery(new_shape, dtype=previous_state.dtype)
-
         return new_state, mem_allocation
