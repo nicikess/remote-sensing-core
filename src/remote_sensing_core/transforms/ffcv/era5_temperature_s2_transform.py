22a517e76b63ff5c7e96fc92dbfafb71021e580f
Nicolas Kesseli <nicolaskesseli@gmail.com>
2023-05-23 18:32:14 +0200

    Change batch size

--- src/remote_sensing_core/transforms/ffcv/era5_temperature_s2_transform.py
+++ src/remote_sensing_core/transforms/ffcv/era5_temperature_s2_transform.py
@@ -9,21 +9,28 @@ from ffcv.pipeline.state import State
 from remote_sensing_core.constants import TEMPERATURE_S2_INDEX
 
 class Era5TemperatureS2Transform(Operation):
-    def __init__(self):
+
+    def __init__(
+        self,
+        batch_size: int,
+    ):
         # values taken from ben-ge_era-5.csv from ben-ge-100
         self.max_value = 304.03
         self.min_value = 261.625
+        self.batch_size = batch_size
+
 
     def generate_code(self) -> Callable:
         # get local variables to use in return function
         max_value = self.max_value
         min_value = self.min_value
+        batch_size = self.batch_size
 
         def era5_transform(row, *args):
             # access temperature s2 at index 3
             temperature_s2 = row[:,TEMPERATURE_S2_INDEX]
             normalized_temperature = (temperature_s2 - min_value) / (max_value - min_value)
-            normalized_temperature = normalized_temperature.reshape((32,1))
+            normalized_temperature = normalized_temperature.reshape((batch_size,1))
             return normalized_temperature
 
         return era5_transform
